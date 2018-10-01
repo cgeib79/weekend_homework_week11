9f7bfbd46574e26de384a427a55648512294a906
Christian Geib <christian.geib79@gmail.com>
2018-10-01 09:58:34 +0100

    second commit -adding flight

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,11 @@
 public class Flight {
+    private int number;
+
+            public Flight(int number){
+            this.number = number;
+            }
+
+    public int hasFlightNumber() {
+    return this.number;
+    }
 }
--- src/test/java/TestFlight.java
+++ src/test/java/TestFlight.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestFlight {
+    Flight flight1;
+    Flight flight2;
+
+    @Before
+    public void before() {
+        flight1 = new Flight(1, "GLA", "Boing 747");
+        flight2 = new Flight(2, "EDI", "Boing 737");
+    }
+
+    @Test
+    public void hasFlightNumber(){
+    assertEquals(1, flight1.hasFlightNumber());
+    }
+
+
+
 }
